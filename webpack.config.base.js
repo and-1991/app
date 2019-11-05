ec2703f34d62e371128541f0a2996d9efb7879d7
darkdemon91 <43109050+darkdemon91@users.noreply.github.com>
2019-11-05 12:33:46 +0300

    Update webpack.config.base.js

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -3,7 +3,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js?$/,
+                test: /\.(js|mjs|jsx|ts|tsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
